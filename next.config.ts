2646132fe991081fb626e75af9a7b354cfea38a4
BadiiMsalmi <msalmi.abdelbadii@tek-up.de>
2025-11-19 14:46:21 +0000

    Revert "Merge branch 'websocket' into 'main'"
    This reverts merge request !7

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,8 @@
-// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    buildActivity: false,
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/appointments/:path*',
-        destination: 'http://localhost:8000/api/appointments/:path*',
-      },
-    ];
-  },
+  /* config options here */
+  devIndicators: false
 };
 
 export default nextConfig;
